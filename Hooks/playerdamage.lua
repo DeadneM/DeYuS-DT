f470b47c56860601bde59915005809e15126f2d1
DeadneM <deadnem@gmail.com>
2020-03-09 16:13:23 +0100

    Better low health shaking by drnewbie

--- Hooks/playerdamage.lua
+++ Hooks/playerdamage.lua
@@ -66,14 +66,21 @@ function PlayerDamage:consume_armor_stored_health(amount)
 end
 
 --Camera Shaking when low health
-Hooks:PostHook(PlayerDamage, "update", "F_"..Idstring("PostHook:PlayerDamage:update:Low Health Shake"):key(), function(self)
+Hooks:PostHook(PlayerDamage, "update", "F_"..Idstring("PostHook:PlayerDamage:update:Low Health Shake"):key(), function(self, _, _, __dt)
 	if not self.__ext_camera then
 		self.__ext_camera = self._unit:camera()
+		self.__low_hp_shake_dt = nil
 	else
-		if self:health_ratio() < 0.3 and managers.player:upgrade_value("player", "max_health_reduction", 1) then
-			self.__ext_camera:play_shaker("player_taser_shock", math.clamp((0.3 - self:health_ratio()), 0.001, 0.295)*3.5, 10)
-		elseif self:health_ratio() < 1 and not managers.player:upgrade_value("player", "max_health_reduction", 1) then
-			self.__ext_camera:play_shaker("player_taser_shock", math.clamp((1 - self:health_ratio()), 0.001, 0.995)*3.5, 10)
+		if not self.__low_hp_shake_dt and self:health_ratio() + 0.0011 < self._max_health_reduction then
+			self.__low_hp_shake_dt = 0.7 + math.random()/10
+			self.__ext_camera:play_shaker("player_taser_shock", 
+				math.clamp(1 - (self:health_ratio()/self._max_health_reduction), 0.001, 0.995)*3.5, 10)
+		end
+		if self.__low_hp_shake_dt then
+			self.__low_hp_shake_dt = self.__low_hp_shake_dt - __dt
+			if self.__low_hp_shake_dt < 0 then
+				self.__low_hp_shake_dt = nil
+			end
 		end
 	end
 end)
