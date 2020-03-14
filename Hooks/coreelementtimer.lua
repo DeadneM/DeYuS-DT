5680c165e3662933b384ddda1575668a28ef1551
DeadneM <deadnem@gmail.com>
2020-03-14 21:29:24 +0100

    cookprintfastertypofix

--- Hooks/coreelementtimer.lua
+++ Hooks/coreelementtimer.lua
@@ -7,14 +7,14 @@ if (Network or not Network:is_server()) and not ElementTimer and not ElementTime
 	return
 end
 --Faster Bile on cook meth
-if (Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") then
-	Hooks:PostHook(ElementTimer, "timer_operation_reset", "CookFaster_timer_operation_reset", function(self)
+Hooks:PostHook(ElementTimer, "timer_operation_reset", "CookFaster_timer_operation_reset", function(self)
+	if (Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") then
 		if (self._id == 102164 or self._id == 102166 or self._id == 102177 or self._id == 102178) then
 			self._timer = self:get_random_table_value_float(1)
 			self:_update_digital_guis_timer()
 		end
-	end)
-end
+	end
+end)
 --Faster print
 if Global.game_settings.level_id == "pal" then
 	Hooks:PostHook(ElementTimer, "on_script_activated", "PrintFaster_HackInitTime", function(self)
@@ -22,13 +22,11 @@ if Global.game_settings.level_id == "pal" then
 			self:timer_operation_set_time(3)
 		end
 	end)
-	
 	Hooks:PostHook(ElementTimer, "on_executed", "PrintFaster_HackSetTime_1", function(self)
 		if self._id == 102736 then
 			self:timer_operation_set_time(3)
 		end
 	end)
-	
 	Hooks:PostHook(ElementTimerOperator, "on_executed", "PrintFaster_HackSetTime_2", function(self)
 		if self._id == 102737 then
 			local element = self:get_mission_element(102736)
--- Hooks/coremissionscriptelement.lua
+++ Hooks/coremissionscriptelement.lua
@@ -4,32 +4,32 @@ core:import("CoreCode")
 core:import("CoreClass")
 MissionScriptElement = MissionScriptElement or class()
 
-if (Network or not Network:is_server()) and not ElementTimer and not ElementTimerOperator and not Global.game_settings then
+if (Network or not Network:is_server()) and not Global.game_settings then
 	return
 end
 --Faster cook
-if Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" then
-	Hooks:PreHook(MissionScriptElement, "_calc_base_delay", "CookFaster_calc_base_delay", function(self)
-			if self._id == 102197 then
-				self._values.base_delay_rand = nil
-				self._values.base_delay = 3
-			end
-	end)
-end
-if ((Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") and self._id == 100724) or 
-	(Global.game_settings.level_id == "mex_cooking" and self._editor_name == "counter_below3") or 
-	((Global.game_settings.level_id == "mia_1" or Global.game_settings.level_id == "crojob2") and self._editor_name == "timer_to_next") then
-	Hooks:PreHook(MissionScriptElement, "_calc_element_delay", "CookFaster_calc_element_delay", function(self, params)
+Hooks:PreHook(MissionScriptElement, "_calc_base_delay", "CookFaster_calc_base_delay", function(self)
+	if Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1" then
+		if self._id == 102197 then
+			self._values.base_delay_rand = nil
+			self._values.base_delay = 3
+		end
+	end
+end)
+Hooks:PreHook(MissionScriptElement, "_calc_element_delay", "CookFaster_calc_element_delay", function(self, params)
+	if ((Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") and self._id == 100724) or 
+		(Global.game_settings.level_id == "mex_cooking" and self._editor_name == "counter_below3") or 
+		((Global.game_settings.level_id == "mia_1" or Global.game_settings.level_id == "crojob2") and self._editor_name == "timer_to_next") then
 		params.delay_rand = nil
 		params.delay = 3
 		return 3
-	end)
-end
-if (Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") then
-	Hooks:PostHook(MissionScriptElement, "on_script_activated", "F_"..Idstring("PostHook:MissionScriptElement:on_script_activated:Enable chance of methlab in basement"):key(), function(self)
+	end
+end)
+Hooks:PostHook(MissionScriptElement, "on_script_activated", "F_"..Idstring("PostHook:MissionScriptElement:on_script_activated:Enable chance of methlab in basement"):key(), function(self)
+	if (Global.game_settings.level_id == "rat" or Global.game_settings.level_id == "alex_1") then
 		if self._id == 100486 and self._values and not self._values.enabled and not self._values.__re_enabled then
 			self._values.__re_enabled = true
 			self._values.enabled = true
 		end
-	end)
-end
+	end
+end)
