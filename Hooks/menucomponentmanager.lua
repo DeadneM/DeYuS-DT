28e464548d04e857860e976438fd849efa6fcc2d
DeadneM <deadnem@gmail.com>
2020-03-20 14:31:29 +0100

    fragtane filters and chat update when offline

--- Hooks/menucomponentmanager.lua
+++ Hooks/menucomponentmanager.lua
@@ -1,32 +1,61 @@
 --Hide rectangle info on main menu
 --Use mainmenustreamliner
---MenuComponentManager.create_new_heists_gui = function(self, node) return end
---MenuComponentManager._create_player_profile_gui = function(self, node) return end
---MenuComponentManager.Holo:Post(BLTNotificationsGui) = function(self) return end
+-- MenuComponentManager.create_new_heists_gui = function(self, node) return end
+-- MenuComponentManager._create_player_profile_gui = function(self, node) return end
+-- MenuComponentManager.Holo:Post(BLTNotificationsGui) = function(self) return end
 
---Not sure about how those are helpfull
---Fix other functions that use MenuCallbackHandler:is_multiplayer
-local DontBlockChat = true
-Hooks:PreHook(MenuComponentManager, "_create_chat_gui", "_create_chat_gui_whenoff", function(self)
-	if Global.game_settings.single_player and not DontBlockChat then
-		return
+--Chat ingame_waiting_for_players
+--Only changed check
+function MenuComponentManager:_create_chat_gui()
+	if SystemInfo:platform() == Idstring("WIN32") and (MenuCallbackHandler:is_multiplayer() or DontBlockChat) and managers.network:session() then
+		self._preplanning_chat_gui_active = false
+		self._lobby_chat_gui_active = false
+		self._crimenet_chat_gui_active = false
+
+		if self._game_chat_gui then
+			self:show_game_chat_gui()
+		else
+			self:add_game_chat()
+		end
+
+		self._game_chat_gui:set_params(self._saved_game_chat_params or "default")
+
+		self._saved_game_chat_params = nil
 	end
-end)
+end
+
 --Fix other functions that use MenuCallbackHandler:is_multiplayer
+--Chat in lobby (Crime Spree offline)
 Hooks:PreHook(MenuComponentManager, "_create_lobby_chat_gui", "_create_lobby_chat_gui_whenoff", function(self)
 	if Global.game_settings.single_player and not DontBlockChat then
 		return
 	end
 end)
+
 --Fix other functions that use MenuCallbackHandler:is_multiplayer
+--When opening Crime.net from lobby (currently not possible in single player but why not)
 Hooks:PreHook(MenuComponentManager, "_create_crimenet_chats_gui", "_create_crimenet_chats_gui_whenoff", function(self)
 	if Global.game_settings.single_player and not DontBlockChat then
 		return
 	end
 end)
---Fix other functions that use MenuCallbackHandler:is_multiplayer
-Hooks:PreHook(MenuComponentManager, "_create_preplanning_chats_gui", "_create_preplanning_chats_gui_whenoff", function(self)
-	if Global.game_settings.single_player and not DontBlockChat then
-		return
+
+--Chat during preplanning
+--Only changed check
+function MenuComponentManager:_create_preplanning_chats_gui()
+	if SystemInfo:platform() == Idstring("WIN32") and (MenuCallbackHandler:is_multiplayer() or DontBlockChat) and managers.network:session() then
+		self._preplanning_chat_gui_active = true
+		self._crimenet_chat_gui_active = false
+		self._lobby_chat_gui_active = false
+
+		if self._game_chat_gui then
+			self:show_game_chat_gui()
+		else
+			self:add_game_chat()
+		end
+
+		self._game_chat_gui:set_params(self._saved_game_chat_params or "preplanning")
+
+		self._saved_game_chat_params = nil
 	end
-end)
+end
