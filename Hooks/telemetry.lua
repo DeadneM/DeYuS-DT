ff1a36b1a9f0461d50ae0599d0f4cdd69cb8d62d
DeadneM <deadnem@gmail.com>
2021-03-21 08:44:08 +0100

    Disable telemetry

---
+++ Hooks/telemetry.lua
@@ -0,0 +1,56 @@
+--Disable Telemetry
+
+local base_url = "http://localhost/"
+
+local function get_geolocation()
+	return
+end
+
+local function get_total_playtime()
+	return
+end
+
+local function update_total_playtime(new_playtime)
+	return
+end
+
+local function send_telemetry(telemetry_body)
+	return
+end
+
+local function send_telemetry(telemetry_body)
+	return
+end
+
+
+local base_url = "http://localhost/"
+
+local function get_geolocation()
+	return
+end
+
+local function get_total_playtime()
+	return
+end
+
+local function update_total_playtime(new_playtime)
+	return
+end
+
+local function send_telemetry(telemetry_body)
+	return
+end
+
+local function send_telemetry(telemetry_body)
+	return
+end
+
+-- Dom's fix
+local function function_empty()
+    return
+end
+
+local Telemetry = Telemetry
+for k, _ in pairs(Telemetry) do
+    Telemetry[k] = function_empty
+end
